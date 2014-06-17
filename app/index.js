c540894df6a2d5a44317955692d8dfb3fbf5ec1c
Ben Foxall <ben@whiteoctober.co.uk>
2014-06-17 18:56:29 +0100

    hook up socket.io

--- app/index.js
+++ app/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000);
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
