c968d7240e468a213ca1b924be95d30f2dfb333f
Ben Foxall <ben@whiteoctober.co.uk>
2014-06-17 19:10:30 +0100

    server side world representation

--- app/index.js
+++ app/index.js
@@ -5,6 +5,19 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+// todo - use buffer
+var world = new Array(100*100);
+
+setInterval(function(){
+  for (var i = world.length - 1; i >= 0; i--) {
+    world[i] = Math.random() > .5;
+  }
+
+  io.emit('state', {world:world});
+}, 1000)
+
+
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
