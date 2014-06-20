17b5fac92935709b444b9396679e1508f02dd8a1
Ryan Brooks <ryanbrooksis@gmail.com>
2014-06-20 22:30:35 +0100

    Adding listeners for client interaction

--- app/index.js
+++ app/index.js
@@ -3,6 +3,24 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// Set up PubNub bits
+var pubnub = require("pubnub").init({
+    publish_key   : "pub-c-3819ce76-a6c1-4514-9d89-fc9808fec1df",
+    subscribe_key : "sub-c-e45a82c6-f844-11e3-bafe-02ee2ddab7fe"
+});
+
+// Whenever we get a new client, add it to the list
+var sessions = [];
+pubnub.subscribe({
+    channel  : "cf_gol_capabilities",
+    callback : function(message) {
+      message.lastContact = new Date();
+      sessions[message.processUUID + '-' + message.responderId] = message;
+      console.log("----");
+      console.log(sessions);
+    }
+});
+
 app.use(express.static(__dirname + '/public'));
 
 // todo - use buffer
@@ -13,7 +31,10 @@ setInterval(function(){
     world[i] = Math.random() > .5;
   }
 
-  io.emit('state', {world:world});
+  io.emit('state', {
+    world:world,
+    players:sessions
+    });
 }, 1000)
 
 
