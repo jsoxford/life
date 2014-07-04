f99edfd8f614a4a9491d576b640b501e5b3c26ff
Ryan Brooks <ryanbrooksis@gmail.com>
2014-07-04 21:08:00 +0100

    Loadsachanges. Moving to object model, for pain and profit

--- index.js
+++ index.js
@@ -15,187 +15,233 @@ app.use(express.static(__dirname + '/public'));
 
 // the connected clients that will increment the game together
 var clients = {}; // id: socket
+var clientWorlds = {}; // id, world
+
 var clientStats = {
-    testsRun: 0,
-    testsFailed: 0,
-    testsIgnored: 0
+	testsRun: 0,
+	testsFailed: 0,
+	testsIgnored: 0
 };
-var client_id = 0, problem_id = 0;
+var client_id = 0,
+	problem_id = 0,
+  generationId = 0;
 
 var statsFile = 'stats.log';
 
 var width = 20,
-    height = 20;
+	height = 20;
 
 // todo - use buffer
-var world = new Array(width*height);
+var world = new Array();
 
-for (var i = world.length - 1; i >= 0; i--) {
-  world[i] = Math.random() > .5;
+for (var x = 0; x < width; x++) {
+  for (var y = 0; y < height; y++) {
+    if(Math.random() > .5){
+      world.push({x: x, y:y});
+    }
+  }
 }
-// console.log(world.length)
-
 
 // read and write parts of the world
 // world, w, h are set
-function writeSquare(data, x, y, w, h){
-  for(var i = 0; i < w; i++){
-    for(var j = 0; j < h; j++){
-      world[ i+x + ((j+y)*width)] = !! data[i + (j*w)];
-    }
-  }
-}
+// function writeSquare(data, x, y, w, h){
+//   for(var i = 0; i < w; i++){
+//     for(var j = 0; j < h; j++){
+//       world[ i+x + ((j+y)*width)] = !! data[i + (j*w)];
+//     }
+//   }
+// }
 
-function readSquare(x, y, w, h){
-  var data = new Array(w*h);
-  for(var i = 0; i < w; i++){
-    for(var j = 0; j < h; j++){
-      data[i + (j*w)] = world[i+x + ((j+y)*width)];
-    }
-  }
-  return data;
+function readSquare(x, y, w, h) {
+	var data = new Array(w * h);
+	for (var i = 0; i < w; i++) {
+		for (var j = 0; j < h; j++) {
+			data[i + (j * w)] = world[i + x + ((j + y) * width)];
+		}
+	}
+	return data;
 }
 
 
 
 // this maps responses to where the world should be updated
 var populator = {
-  /* g0:{x:0, y:0, width:5, height:5 } */
+	/* g0:{x:0, y:0, width:5, height:5 } */
 }
 
 
 
 var server = net.createServer(function(c) {
-  var id = client_id++;
-  clients[id] = c;
+	var id = client_id++;
+	clients[id] = c;
+  clientWorlds[id] = world.slice(0);
 
-  console.log('client ' + id + ' connected');
+	console.log('client ' + id + ' connected');
 
-  c.on('end', function() {
-    delete clients[id];
-    console.log('client ' + id + ' disconnected');
-  });
+	c.on('end', function() {
+		delete clients[id];
+    delete clientWorlds[id];
+		console.log('client ' + id + ' disconnected');
+	});
 
-  c.pipe(JSONStream.parse(true))
-  .pipe(es.mapSync(process))
+	c.pipe(JSONStream.parse(true))
+		.pipe(es.mapSync(function (data) {
+      process(id, data)
+		}))
 
 });
 
 server.listen(8787, function() {
-  console.log('listening for clients on *:8787');
+	console.log('listening for clients on *:8787');
 });
 
 
 // process a message from the client
-function process(data){
-  if(data.respondingTo === 'processIteration'){
-      var iterationId = data.payload[1].generation;
-      var iterationResult = data.payload[1].result;
 
-    var p = data.payload[1].result || {};
+function process(clientId, data) {
+	try {
+		if (data.success === false) {
+			// We'll not worry and just ignore it for now
+		}
+    else if(data.respondingTo === 'tickBoard'){
+      var latestResult = data.payload[0].generation > data.payload[1].generation ? 0 : 1;
+      var generationId = data.payload[latestResult].generation;
 
-    io.emit('state', {
-      world:world,
-      clientStats:clientStats
-    });
+      var tickResult = data.payload[latestResult].result;
+      var lastTick = clientWorlds[clientId].slice(0);
+      var nextTick = tickBoard(lastTick);
 
-    // Object.keys(p).forEach(function(k){
-    //
-    //   var populate = populator[k];
-    //   var solution = p[k].split('').map(function(d){return d === '1'});
-    //
-    //   if(populate){
-    //     writeSquare(solution, populate.x, populate.y, populate.width, populate.height)
-    //   }
-    //
-    //   // clean up
-    //   delete populator[k]
-    // })
-  }
-  else if(data.action === 'consumeTestResults'){
-    clientStats.testsRun += data.payload.testsRun;
-    clientStats.testsFailed += data.payload.testsFailed;
-    clientStats.testsIgnored += data.payload.testsIgnored;
+      // validate against reference
+      //FIXME!
 
-    io.emit('state', {
-      world:world,
-      clientStats:clientStats
-    });
-
-    // Dump the stats to a file for later
-    var logMsg = new Date().getTime() + ': ' + JSON.stringify(data.payload) + '\n';
-    fs.exists(statsFile, function(exists) {
-    if (!exists) {
-        fs.writeFile(statsFile, '');
+      // update reference
+      clientWorlds[clientId] = nextTick;
     }
-    fs.appendFile(statsFile, logMsg, function (err) {
-        if(err){
-            console.error("Writing to the stats log file failed. This isn't catastrophic, but stats are nice.");
-        }
-    });
-});
+    else if(data.respondingTo === 'tickCell'){
+      var latestResult = data.payload[0].generation > data.payload[1].generation ? 0 : 1;
+      var generationId = data.payload[latestResult].generation;
+      var tickResult = data.payload[latestResult].result;
+      console.dir(clientId + ':' + generationId + " => "+ tickResult);
 
-  }
-  else{
-    console.log('Unknown message received:');
-    console.log(data);
-  }
+
+    }
+    else if (data.respondingTo === 'processIteration') {
+			var iterationId = data.payload[1].generation;
+			var iterationResult = data.payload[1].result;
+
+			var p = data.payload[1].result || {};
+
+			io.emit('state', {
+				world: world,
+				clientStats: clientStats
+			});
+
+			// Object.keys(p).forEach(function(k){
+			//
+			//   var populate = populator[k];
+			//   var solution = p[k].split('').map(function(d){return d === '1'});
+			//
+			//   if(populate){
+			//     writeSquare(solution, populate.x, populate.y, populate.width, populate.height)
+			//   }
+			//
+			//   // clean up
+			//   delete populator[k]
+			// })
+		}
+    else if (data.action === 'consumeTestResults') {
+			clientStats.testsRun += data.payload.testsRun;
+			clientStats.testsFailed += data.payload.testsFailed;
+			clientStats.testsIgnored += data.payload.testsIgnored;
+
+			io.emit('state', {
+				world: world,
+				clientStats: clientStats
+			});
+
+			// Dump the stats to a file for later
+			var logMsg = new Date().getTime() + ': ' + JSON.stringify(data.payload) + '\n';
+			fs.exists(statsFile, function(exists) {
+				if (!exists) {
+					fs.writeFile(statsFile, '');
+				}
+				fs.appendFile(statsFile, logMsg, function(err) {
+					if (err) {
+						console.error("Writing to the stats log file failed. This isn't catastrophic, but stats are nice.");
+					}
+				});
+			});
+		} else {
+			console.log('Unknown message received:');
+			console.log(data);
+		}
+	} catch (e) {
+		// Something went wrong, it was probably the input from the client. Crack on!
+		console.log('Unknown message received:');
+		console.log(data);
+    console.error(e);
+	}
 }
 
-function dispatch(client, x, y, w, h){
+function tickEverything(generationId){
+  var activeClients = Object.keys(clients).filter(function (clientId) { return clientId != null; })
 
-  if(!clients.hasOwnProperty(client)) return;
+  // Send out tick request for each client's board
+  activeClients.forEach(function (clientId) {
+    sendCommand(clientId, 'tickBoard', {
+      generation: generationId,
+      result: clientWorlds[clientId]
+    });
+  });
 
-  var id = 'g' + problem_id++;
-  populator[id] = {
-    x:x,
-    y:y,
-    width:w,
-    height:h
-  };
+  // Send out tick request for each cell
+  // FIXME
 
-  setTimeout(function(){
-    delete populator[id]
-  }, 5000)
+}
 
+function sendCommand(clientId, action, payload){
   var request = {
-    action: "processIteration",
-    payload: {
-        generation: problem_id,
-        result: readSquare(x, y, w, h).map(function(x){return x ? '1':'0'}).join('')
+      action: action,
+      payload: payload
     }
-  }
-  clients[client].write(JSON.stringify(request) + '\n');
+  clients[clientId].write(JSON.stringify(request) + '\n');
 }
 
+io.on('connection', function(socket) {
+	socket.emit('state', {
+		world: world,
+		clientStats: clientStats
+	});
 
-io.on('connection', function (socket) {
-  socket.emit('state', {
-    world:world,
-    clientStats: clientStats
-  });
-
-  socket.on('on', function (data) {
-    var x = data[0], y = data[1];
-    var i = x + (y*width);
-    if(world[i] !== undefined){
-      world[i] = false;
-    }
-  });
+	socket.on('on', function(data) {
+		var x = data[0],
+			y = data[1];
+		var i = x + (y * width);
+		if (world[i] !== undefined) {
+			world[i] = false;
+		}
+	});
 
 });
 
 
 // Repeatedly ask for the next generation
-setInterval(function(){
-  // ask a player for a solution to part of the world
-  var player = Object.keys(clients)[0];
-  if(player != null){
-    dispatch(player, 0, 0, 10, 10);
-  }
+setInterval(function () {
+  tickEverything(generationId);
+  generationId++;
 }, 1000)
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function() {
+	console.log('listening on *:3000');
 });
+
+
+// Reference implementations of client ticks
+function tickBoard(lastTick){
+  return [{x:1, y:1}];
+}
+
+function tickCell(lastTick){
+  return lastTick;
+}
