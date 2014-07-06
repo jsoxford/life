7c241b1d0729e705b29f49bcf8285491dcb62795
Peter West <pete@whiteoctober.co.uk>
2014-07-06 01:16:39 +0100

    Consistency!

--- referenceClient.js
+++ referenceClient.js
@@ -1,37 +1,36 @@
+exports.tickCell = function (generation0) {
+  var liveCellsIncludingSelf = generation0.match(/1/g) || 0;
+  if (generation0[4] === '1') {
 
-exports.tickCell = function(generation0){
-    var liveCellsIncludingSelf = generation0.match(/1/g) || 0;
-    if(generation0[4] === '1'){
-
-      //Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-      if(liveCellsIncludingSelf.length < 3){
-        return false;
-      }
-
-      //Any live cell with two or three live neighbours lives on to the next generation.
-      else if(liveCellsIncludingSelf.length == 3 || liveCellsIncludingSelf.length == 4){
-        return true;
-      }
-
-      //Any live cell with more than three live neighbours dies, as if by overcrowding.
-      else{
-        return false;
-      }
-
+    // Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+    if (liveCellsIncludingSelf.length < 3) {
+      return false;
     }
-    else{
-      //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-      if(liveCellsIncludingSelf.length == 3){
-        return true;
-      }
-      else{
-        return false;
-      }
-    }
-    return 'x'
-}
 
-exports.tickBoard = function(generation0){
-  //FIXME implement!
+    // Any live cell with two or three live neighbours lives on to the next generation.
+    else if (liveCellsIncludingSelf.length == 3 || liveCellsIncludingSelf.length == 4) {
+      return true;
+    }
+
+    // Any live cell with more than three live neighbours dies, as if by overcrowding.
+    else {
+      return false;
+    }
+
+  }
+  else{
+    // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+    if (liveCellsIncludingSelf.length == 3) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  return 'x';
+};
+
+exports.tickBoard = function (generation0) {
+  // FIXME implement!
   return generation0;
-}
+};
